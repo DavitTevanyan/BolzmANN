a29a698f9c227d6dbdd46933e0611d6f53ba60b8
Leo Heinsaar <leo.heinsaar@hotmail.com>
2016-11-16 15:41:01 +0400

    Indentation.

--- ANN/Neuron.h
+++ ANN/Neuron.h
@@ -5,40 +5,42 @@
 
 namespace ANN
 {
-    class   Neuron;
-    struct  Connection;
-    typedef std::vector<Neuron> Layer;
 
-    class Neuron
-    {
-    public:
-               Neuron(int numOutputs, int myIdxL);
-               void   activate(const Layer& prevLayer);
-               void   calcOutputGradients(const double target);
-               void   calcHiddenGradients(const Layer& nextLayer);
-               void   updateInputWeights(Layer& prevLayer);
-               void   setOutput(double val) { output_ = val;  }
-               double output() const        { return output_; }
-    private:
-        static double activationFunction(double x);           // for forward  propagation
-        static double activationFunctionDerivative(double x); // for backward propagation
+class   Neuron;
+struct  Connection;
+typedef std::vector<Neuron> Layer;
 
-               double sumDOW(const Layer& nextLayer) const;
+class Neuron
+{
+public:
+           Neuron(int numOutputs, int myIdxL);
+           void   activate(const Layer& prevLayer);
+           void   calcOutputGradients(const double target);
+           void   calcHiddenGradients(const Layer& nextLayer);
+           void   updateInputWeights(Layer& prevLayer);
+           void   setOutput(double val) { output_ = val;  }
+           double output() const        { return output_; }
+private:
+    static double activationFunction(double x);           // for forward  propagation
+    static double activationFunctionDerivative(double x); // for backward propagation
 
-        static double rate;     // [0.0, 1.0] overall net learning rate
-        static double momentum; // [0.0,   n] multiplier of last weight change (momentum)
-               double output_;
-               double gradient_;
-        const  int    idxL_;
+           double sumDOW(const Layer& nextLayer) const;
 
-        std::vector<Connection> axon_;
-    };
+    static double rate;     // [0.0, 1.0] overall net learning rate
+    static double momentum; // [0.0,   n] multiplier of last weight change (momentum)
+           double output_;
+           double gradient_;
+    const  int    idxL_;
 
-    struct Connection
-    {
-        double weight = rand() / double(RAND_MAX);
-        double deltaWeight;
-    };
+    std::vector<Connection> axon_;
+};
+
+struct Connection
+{
+    double weight = rand() / double(RAND_MAX);
+    double deltaWeight;
+};
+
 }
 
 #endif // NEURON_H
