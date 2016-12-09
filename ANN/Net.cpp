370ddb3a6cce2cd9d61132513bfee25f7f3008bf
DavitTevanyan <davittevanian@gmail.com>
2016-12-09 18:16:09 +0400

    Add existing file

--- ANN/Net.cpp
+++ ANN/Net.cpp
@@ -7,126 +7,149 @@ using namespace ANN;
 
 double Ann::averageSmoothingFactor_ = 100.0; // Number of training samples to average over
 
-Ann::Ann(const std::vector<int>& layers)
-    : error_(0.0), averageError_(0.2)
+Ann::Ann(const std::vector<int>& topology) 
+    : error_(0.0),
+    averageError_(0.2),
+    topology_(topology)
 {
-    const size_t numLayers = layers.size();
-
-    for (int L = 0; L < numLayers; ++L)
+    int neuronsCount = 0;
+    for (size_t i = 0; i < topology_.size(); ++i)
     {
-        layers_.emplace_back(Layer());
+        neuronsCount += topology_[i] + 1;
+    }
 
-        int outs = 0;
-        if (L < numLayers - 1)
-            outs = layers[L + 1];
+    bool isBias;
+    int  n     = 0;
+    int  index = 0;
+    std::vector<int> in;
+    std::vector<int> out;
 
-        // Fill layer with neurons; last neuron is bias
-        for (int posL = 0; posL <= layers[L]; ++posL)
-        {
-            layers_[L].emplace_back(Neuron(posL, outs));
+    for (int L = 0; L < topology_.size(); ++L)
+    {
+        isBias = false;
+        index += topology_[L] + 1; 
+        for (; n < neuronsCount - 1; ++n)
+        {           
+            if (L == 0 || L != topology_.size() - 1)
+            {
+                for (int i = 0; i < topology_[L + 1]; ++i)
+                {
+                    out.emplace_back(index + i);
+                }
+            }
+            if ((L == topology_.size() - 1 || L != 0) && (n != index - 1))
+            {
+                for (int i = 0; i < topology_[L - 1] + 1; ++i)
+                {
+                    in.emplace_back(index - topology_[L] - topology_[L - 1] - 2 + i);
+                }
+            }
+            if (n == index - 1)
+                isBias = true;
+            net_.emplace_back(Neuron(in, out, isBias));
+            in.clear();
+            out.clear();
+
+            if (n == index - 1)
+            {
+                ++n;
+                break;
+            }
         }
-
-        // Bias is last neuron, fixed output
-        layers_[L].back().setOutput(1.0); // completely irrelevant for L == numLayers-1
+        if (L != topology_.size() - 1)
+        {
+            net_.back().setOutput(1.0);
+        }
     }
 }
 
 void Ann::feedForw(const std::vector<double>& worldInput)
 {
-    assert(worldInput.size() == layers_[INPUT].size() - 1);
+    assert(worldInput.size() == topology_[INPUT]);
 
     // Assign world input values to input neurons
     for (int n = 0; n < worldInput.size(); ++n)
-    {
-        layers_[INPUT][n].setOutput(worldInput[n]);
-    }
+        net_[n].setOutput(worldInput[n]);
 
     // Forward propagate: activate every neuron
-    // in every layer except input layer neurons
-    for (int L = 1; L < layers_.size(); ++L)
-    {
-        Layer& prevLayer  = layers_[L - 1];
-        for (int n = 0; n < layers_[L].size() - 1; ++n)
-        {
-            layers_[L][n].activate(prevLayer);
-        }
-    }
+    // in every layer except input layer neurons   
+    for (int n = 0; n < net_.size(); ++n)
+        net_[n].activate(net_, n);
 }
 
 void Ann::backProp(const std::vector<double>& target)
 {
     // Calculate overall net error (RMS of output neuron errors)
-    ANN::Layer& outputLayer = layers_.back();
-    error_ = 0.0; // reset for each backpropagation
-
-    for (int n = 0; n < outputLayer.size() - 1; ++n) 
+    int neuronsCount = 0;
+    for (size_t i = 0; i < topology_.size(); ++i)
     {
-        double delta = target[n] - outputLayer[n].output();
-        error_ += delta * delta;
+        neuronsCount += topology_[i] + 1;
     }
-    error_ /= outputLayer.size() - 1; // average
-    error_ = sqrt(error_);            // RMS
+
+    error_ = 0.0; // reset for each backpropagation
+    int n = neuronsCount - topology_[topology_.size() - 1] - 1;
+    int m = 0;
+    for (; n < net_.size(); ++n)
+    {
+        double delta = target[m++] - net_[n].output();
+        error_ += delta * delta;        
+    }
+
+    error_ /= topology_[topology_.size() - 1]; // average
+    error_ = sqrt(error_);                 // RMS
 
     // Implement a recent average measurement [for displaying, not related to learning]
     averageError_ = (averageError_ * averageSmoothingFactor_ + error_)
                   / (                averageSmoothingFactor_ + 1.0);
 
     // Calculate output layer gradients
-    for (int n = 0; n < outputLayer.size() - 1; ++n) 
+    n = neuronsCount - topology_[topology_.size() - 1] - 1;
+    m = 0;
+    for (; n < net_.size(); ++n)
     {
-        outputLayer[n].calcOutputGradients(target[n]);
+        net_[n].calcOutputGradients(target[m++]);
     }
 
     // Calculate hidden layer gradients
-    for (size_t i = layers_.size() - 2; i > 0; --i) 
-    {
-        Layer& hiddLayer = layers_[i];
-        Layer& nextLayer = layers_[i + 1];
-
-        for (auto& neuron : hiddLayer) 
-        {
-            neuron.calcHiddenGradients(nextLayer);
-        }
-    }
+    
+    for (int n = net_.size() - topology_[topology_.size() - 1] - 1; n > topology_[0]; --n)
+        net_[n].calcHiddenGradients(net_);
 
     // Update connection weights
     // for all layers from output to first hidden layer
-    for (size_t i = layers_.size() - 1; i > 0; --i) 
-    {
-        Layer&  prevLayer = layers_[i - 1];
-        for (int n = 0; n < layers_[i].size() - 1; ++n) 
-        {
-            layers_[i][n].updateInputWeights(prevLayer);
-        }
-    }
+    for (int n = 0; n < net_.size() - topology_[topology_.size() - 1]; ++n)
+        net_[n].updateInputWeights(net_);
 }
 
 std::vector<double> Ann::getOutput() const
 {
     std::vector<double> result;
 
-    for (const auto& neuron : layers_.back())
-        result.push_back(neuron.output());
+    int neuronsCount = 0;
+    for (size_t i = 0; i < topology_.size(); ++i)
+    {
+        neuronsCount += topology_[i] + 1;
+    }
 
-    result.pop_back(); // remove bias neuron
-
+    int n = neuronsCount - topology_[topology_.size() - 1] - 1;
+    for (; n < net_.size(); ++n)
+    {
+        result.emplace_back(net_[n].output());
+    }
+  
     return result;
 }
 
 void Ann::reportState(const std::string& fileName)
 {
     std::stringstream ss;
-    for (size_t i = 0; i < layers_.size() - 1; ++i)
+
+    for (int n = 0; n < net_.size(); ++n)
     {
-        Layer& currLayer = layers_[i];
-        Layer& nextLayer = layers_[i + 1];
-        for (auto& neuron : currLayer)
-        {
-            ss << neuron.reportState(nextLayer);
-        }
-        ss << "===================================="
-           << std::endl;
+        net_[n].reportState();
     }
+    ss << "===================================="
+        << std::endl;
 
     std::ofstream(fileName) << ss.rdbuf();
 }
