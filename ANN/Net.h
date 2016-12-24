03abfe6713964c91a9846b8adb449cb5377134f0
DavitTevanyan <davittevanian@gmail.com>
2016-12-24 16:17:19 +0400

    Support for adding or removing neuron or connection into/from network

--- ANN/Net.h
+++ ANN/Net.h
@@ -12,10 +12,17 @@ namespace ANN {
         explicit  Ann(const std::vector<int>& topology);
         void feedForw(const std::vector<double>& worldInput);
         void backProp(const std::vector<double>& target);
+        void addNeuron(int layer, int neuron, bool isBias);
+        void deleteNeuron(int layer, int neuron);
+        void addConnection(int srcLayer, int srcNeuron, int dstLayer, int dstNeuron);
+        void deleteConnection(int srcLayer, int srcNeuron, int dstLayer, int dstNeuron);
 
-        std::vector<double> getOutput()    const;
+        std::vector<double> getOutput() const;
         double              averageError() const { return averageError_; }
         void                reportState(const std::string& fileName);
+
+    private:
+        void findIndexes(int srcLayer, int srcNeuron, int dstLayer, int dstNeuron, int& srcIndex, int& dstIndex);
 
     private:
         double              error_;
